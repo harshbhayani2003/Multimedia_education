62293e7924fc3a4df84fce528f49b703bfa8f204
harshbhayani <harshbhayani7853@gmail.com>
2024-09-17 15:24:51 +0530

    Initial commit

---
+++ css/Student-Corner-Css/JAVASCRIPT/placement.js
@@ -0,0 +1,19 @@
+const btnop1= document.getElementById("op-1");
+const btnop2= document.getElementById("op-2");
+const op1Text= document.getElementById("op-1-text");
+const op2Text= document.getElementById("op-2-text");
+
+
+btnop1.onclick = function(){
+    op1Text.style.display = "block";
+    op2Text.style.display = "none";
+    btnop1.style.borderBottom ="2vh solid black";
+    btnop2.style.borderBottom ="none";
+}
+
+btnop2.onclick = function(){
+    op1Text.style.display = "none";
+    op2Text.style.display = "block";
+    btnop1.style.borderBottom ="none";
+    btnop2.style.borderBottom ="2vh solid black";
+}
---
+++ js/Book_Free_Demo.js
@@ -0,0 +1,8 @@
+let btn= document.getElementById('demofrom');
+
+btn.addEventListener('submit',()=>{
+    alert("hello for my sitre")
+})
+
+
+
